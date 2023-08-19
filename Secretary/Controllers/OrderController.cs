ae12efbdda60cc400b5aaaef763710e11608f0bf
ehsan3535 <ehsanjoo35@gmail.com>
2023-08-19 15:51:52 +0330

    aa

--- Secretary/Controllers/OrderController.cs
+++ Secretary/Controllers/OrderController.cs
@@ -17,6 +17,7 @@ using Secretary.Models;
 using Secretary.Models.Orders;
 using Sentry;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -49,9 +50,41 @@ namespace Admin.Controllers
             this.userManager = userManager;
             this.notification = notification;
         }
-        public async Task<IActionResult> OrderList(CancellationToken cancellationToken)
+        public async Task<IActionResult> OrderList(string? Sort, CancellationToken cancellationToken)
         {
-            var model = await Orderripo.TableNoTracking.ProjectTo<OrderDto>(mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+            var model = new List<OrderDto>();
+            if (Sort != null)
+            {
+                if (Sort == "0" )
+                {
+                model = await Orderripo.TableNoTracking.Where(x=>x.PaymentStatus == Entities.Constants.PaymentStatus.Waiting).ProjectTo<OrderDto>(mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+
+                }
+                else if(Sort == "1")
+                {
+                    model = await Orderripo.TableNoTracking.Where(x => x.PaymentStatus == Entities.Constants.PaymentStatus.Payed).ProjectTo<OrderDto>(mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+                }
+                else if (Sort == "2")
+                {
+                    model = await Orderripo.TableNoTracking.Where(x => x.PaymentStatus == Entities.Constants.PaymentStatus.Sending).ProjectTo<OrderDto>(mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+                }
+                else if (Sort == "3")
+                {
+                    model = await Orderripo.TableNoTracking.Where(x => x.PaymentStatus == Entities.Constants.PaymentStatus.Sent).ProjectTo<OrderDto>(mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+                }
+                else if (Sort == "4")
+                {
+                    model = await Orderripo.TableNoTracking.Where(x => x.PaymentStatus == Entities.Constants.PaymentStatus.Gived).ProjectTo<OrderDto>(mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+                }
+                else if (Sort == "5")
+                {
+                    model = await Orderripo.TableNoTracking.Where(x => x.PaymentStatus == Entities.Constants.PaymentStatus.Cash).ProjectTo<OrderDto>(mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+                }
+            }
+            else
+            {
+                model = await Orderripo.TableNoTracking.ProjectTo<OrderDto>(mapper.ConfigurationProvider).ToListAsync(cancellationToken);
+            }
             return View(model);
         }
 
@@ -61,20 +94,20 @@ namespace Admin.Controllers
             model.OrderDetails = await OrderDetailripo.TableNoTracking.Where(x => x.OrderId == OrderId).ProjectTo<OrderDetailDto>(mapper.ConfigurationProvider).ToListAsync(cancellationToken);
             return View(model);
         }
-       /* public async Task<IActionResult> DeleteOrder(Guid Id, CancellationToken cancellationToken)
-        {
-            var model = await Orderripo.TableNoTracking.Where(x => x.Id == Id).FirstOrDefaultAsync(cancellationToken);
-            var orderdateillist = await OrderDetailripo.TableNoTracking.Where(x => x.OrderId == model.Id).ToListAsync(cancellationToken);
+        /* public async Task<IActionResult> DeleteOrder(Guid Id, CancellationToken cancellationToken)
+         {
+             var model = await Orderripo.TableNoTracking.Where(x => x.Id == Id).FirstOrDefaultAsync(cancellationToken);
+             var orderdateillist = await OrderDetailripo.TableNoTracking.Where(x => x.OrderId == model.Id).ToListAsync(cancellationToken);
 
-            foreach (var item in orderdateillist)
-            {
-                item.Add(item);
-            }
-            OrderDetailripo.DeleteRange(NewOrderDetailList);
-            model.TotalPrice = 0;
-            await Orderripo.UpdateAsync(model, cancellationToken);
-            return RedirectToAction(nameof(OrderList));
-        }*/
+             foreach (var item in orderdateillist)
+             {
+                 item.Add(item);
+             }
+             OrderDetailripo.DeleteRange(NewOrderDetailList);
+             model.TotalPrice = 0;
+             await Orderripo.UpdateAsync(model, cancellationToken);
+             return RedirectToAction(nameof(OrderList));
+         }*/
         [HttpPost]
         public async Task<IActionResult> ChangeStatus(OrderDto Dto, CancellationToken cancellationToken)
         {
